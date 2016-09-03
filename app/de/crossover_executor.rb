dff2a9df7b728fcbdc4f39289640ad6d0f2e4965
Toshinori Tsugita <tsugita.dev@gmail.com>
2016-09-03 23:24:16 +0900

    Add exponential crossover option

--- app/de/crossover_executor.rb
+++ app/de/crossover_executor.rb
@@ -1,20 +1,22 @@
 class DE end
 
 class DE::CrossoverExecutor
-  def self.create_children(parent_vectors: nil, mutated_vectors: nil, use_mutated_component_rate: nil)
+  def self.create_children(parent_vectors: nil, mutated_vectors: nil, use_mutated_component_rate: nil, crossover_method: :binomial)
     new(
       parent_vectors: parent_vectors,
       mutated_vectors: mutated_vectors,
-      use_mutated_component_rate: use_mutated_component_rate
+      use_mutated_component_rate: use_mutated_component_rate,
+      crossover_method: :binomial
     ).create_children
   end
 
-  def initialize(parent_vectors: nil, mutated_vectors: nil, use_mutated_component_rate: nil)
+  def initialize(parent_vectors: nil, mutated_vectors: nil, use_mutated_component_rate: nil, crossover_method: :binomial)
     raise 'parent and mutated vectors must be passed' if parent_vectors == nil || mutated_vectors == nil
     raise 'use_mutated_component_rate must be passed' if use_mutated_component_rate == nil
     @parent_vectors = parent_vectors
     @mutated_vectors = mutated_vectors
     @use_mutated_component_rate = use_mutated_component_rate
+    @crossover_method = crossover_method
   end
 
   def create_children
@@ -22,19 +24,51 @@ class DE::CrossoverExecutor
 
     vector_count.times do |i|
       p_v, m_v = @parent_vectors[i], @mutated_vectors[i]
-      child_components = []
 
-      dim.times do |d|
-        child_components[d] = use_mutated_component? ? m_v[d] : p_v[d]
+      crossovered_vector = case @crossover_method
+      when :binomial
+        binomial_crossovered_vector(p_v, m_v)
+      when :exponential
+        exponential_crossovered_vector(p_v, m_v)
+      else
+        raise "crossover method '#{@crossover_method}' is invalid."
       end
 
-      children << Vector.elements(child_components, false)
+      children << crossovered_vector
     end
 
     children
   end
 
   private
+
+  def binomial_crossovered_vector(p_v, m_v)
+    child_components = []
+    must_use_mutated_index = rand(dim) - 1
+
+    dim.times do |d|
+      child_components[d] = if d == must_use_mutated_index || use_mutated_component?
+        m_v[d]
+      else
+        p_v[d]
+      end
+    end
+
+    Vector.elements(child_components, false)
+  end
+
+  def exponential_crossovered_vector(p_v, m_v)
+    child_components = p_v.to_a
+    k, j = 1, rand(dim)
+
+    begin
+      child_components[j] = m_v[j]
+      k += 1
+      j = (j + 1) % dim
+    end while use_mutated_component? && k < dim
+
+    Vector.elements(child_components, false)
+  end
 
   def vector_count
     @vector_count ||= @parent_vectors.size
--- app/utility.rb
+++ app/utility.rb
@@ -1,5 +1,4 @@
 require 'pry'
-require 'libsvm'
 require 'matrix'
 require 'benchmark'
 
