9b4a942c7d66c24987b0408fe785130bc92685c2
Toshinori Tsugita <tsugita.dev@gmail.com>
2016-09-04 22:24:53 +0900

    Call oracle simulation with options

--- app/oracle_simulation.rb
+++ app/oracle_simulation.rb
@@ -4,7 +4,21 @@ require_relative './oracle_simulator/success_checker.rb'
 require_relative './de.rb'
 require_relative './jade.rb'
 
-oracle_function = OracleSimulator::Function::F3.new
+oracle_function = OracleSimulator::Function::F2.new
 success_checker = OracleSimulator::SuccessChecker::F1.new
-de = JADE.new
+de = JADE.new(
+  dimension: 2,
+  number_of_vectors: 50,
+  max_generation: 50000,
+  max_evaluation: 100000,
+  initial_value_min: -100,
+  initial_value_max: 100,
+  initial_magnification_rate_mean: 0.5,
+  initial_use_mutated_component_rate_mean: 0.5,
+  normal_distribution_sigma: 0.1,
+  cauchy_distribution_gamma: 0.1,
+  p_to_use_current_to_pbest_mutation: 0.1,
+  archived_vectors_size: 50,
+  c_to_use_new_rate_mean_weight: 0.05
+)
 de.oracle_simulate(oracle_function, success_checker)
