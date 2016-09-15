df8b80022076b8468af0f05cfc62a1f468931d47
Toshinori Tsugita <tsugita.dev@gmail.com>
2016-09-15 16:14:11 +0900

    Also save fail parameters

--- app/de/parameter_saveable_selection_executor.rb
+++ app/de/parameter_saveable_selection_executor.rb
@@ -1,10 +1,11 @@
 require_relative './selection_executor.rb'
 
 class DE::ParameterSaveableSelectionExecutor < DE::SelectionExecutor
-  attr_reader :success_parameters
+  attr_reader :success_parameters, :fail_parameters
 
   def create_selected_vectors
     @success_parameters = []
+    @fail_parameters = []
     super
   end
 
@@ -12,5 +13,9 @@ class DE::ParameterSaveableSelectionExecutor < DE::SelectionExecutor
 
   def exec_when_child_vector_is_better(p_v, c_v)
     @success_parameters << p_v.parameter
+  end
+
+  def exec_when_parent_vector_is_better(p_v, c_v)
+    @fail_parameters << p_v.parameter
   end
 end
