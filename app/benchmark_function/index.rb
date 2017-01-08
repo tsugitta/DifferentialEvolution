f6651f060cc9cbb018902a2682967db9a86b4b9f
Toshinori Tsugita <tsugita.dev@gmail.com>
2017-01-09 07:15:29 +0900

    Can show the shape of the benchmark function

--- app/benchmark_function/index.rb
+++ app/benchmark_function/index.rb
@@ -2,3 +2,5 @@ require_relative './helper.rb'
 
 require_relative './f1.rb'
 require_relative './f2.rb'
+
+require_relative './shape_viewer.rb'
---
+++ app/benchmark_function/shape_viewer.rb
@@ -0,0 +1,39 @@
+class BenchmarkFunction::ShapeViewer
+  DOT_COUNT_ON_A_POSITIVE_LINE = 20
+
+  def show(f:, max: 30)
+    Gnuplot.open do |gp|
+      Gnuplot::SPlot.new( gp ) do |plot|
+        plot.title f.label
+        plot.xlabel "x"
+        plot.ylabel "y"
+
+        plot.set "dgrid3d 30,30"
+        plot.set "hidden3d"
+        plot.set "style fill  transparent solid 0.60 border"
+        # show 2d mapping
+        # plot.set "view map"
+
+        xs, ys, zs = [], [], []
+
+        d = DOT_COUNT_ON_A_POSITIVE_LINE
+        (-d..d).each do |i|
+          (-d..d).each do |j|
+            x = max.to_f * i / d
+            y = max.to_f * j / d
+            v = Vector[x, y]
+
+            xs << x
+            ys << y
+            zs << f.calc(v)
+          end
+        end
+
+        plot.data << Gnuplot::DataSet.new( [xs, ys, zs] ) do |ds|
+          ds.with = "pm3d"
+          ds.notitle
+        end
+      end
+    end
+  end
+end
