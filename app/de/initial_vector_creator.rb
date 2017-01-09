021150068f0f8adbba374b33d0ccebda15b7e845
Toshinori Tsugita <tsugita.dev@gmail.com>
2017-01-10 00:51:43 +0900

    Fixnum is deprecated in Ruby2.4

--- app/de/initial_vector_creator.rb
+++ app/de/initial_vector_creator.rb
@@ -2,7 +2,7 @@ class DE end
 
 class DE::InitialVectorCreator
   def initialize(dimension: nil, min: nil, max: nil)
-    raise 'dimension: Int must be passed' unless dimension.is_a?(Fixnum)
+    raise 'dimension: Int must be passed' unless dimension.is_a?(Integer)
     raise 'min and max must be passed' if min == nil || max == nil
     raise 'not min < max ' unless min < max
     @dimension = dimension
@@ -11,7 +11,7 @@ class DE::InitialVectorCreator
   end
 
   def create(length)
-    raise 'length: Int must be passed' unless length.is_a?(Fixnum)
+    raise 'length: Int must be passed' unless length.is_a?(Integer)
 
     Array.new(length) do
       Vector.elements \
