5ebb5f8cd719f93a1fab91a0727d8f3ee1ae267f
Toshinori Tsugita <tsugita.dev@gmail.com>
2017-01-12 04:31:31 +0900

    Add validation for parameter

--- app/entities/parameter.rb
+++ app/entities/parameter.rb
@@ -4,6 +4,11 @@ class Parameter
   attr_accessor :calculated_value_diff
 
   def initialize(magnification_rate, use_mutated_component_rate)
+    if [magnification_rate, use_mutated_component_rate].include?(Float::INFINITY) ||
+      magnification_rate.to_f.nan? || use_mutated_component_rate.to_f.nan?
+      raise 'Cannot continue because attributes include INFINITY or NAN'
+    end
+    
     @magnification_rate = magnification_rate
     @use_mutated_component_rate = use_mutated_component_rate
   end
