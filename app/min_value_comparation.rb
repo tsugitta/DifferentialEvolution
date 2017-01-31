21e457908d4e0134a92333cb71675e3673e96e50
Toshinori Tsugita <tsugita.dev@gmail.com>
2017-01-31 14:55:39 +0900

    Rename `min_value _comarer.rb` to `min_value_comparation.rb` as it’s not an executor but an execution

--- app/min_value_comparation.rb
+++ app/min_value_comparation.rb
@@ -16,9 +16,9 @@ fs = [
   BenchmarkFunction::F2.new,
   BenchmarkFunction::F3.new,
   BenchmarkFunction::F4.new,
-  BenchmarkFunction::F5.new,
-  BenchmarkFunction::F6.new,
-  BenchmarkFunction::F8.new
+  # BenchmarkFunction::F5.new,
+  # BenchmarkFunction::F6.new,
+  # BenchmarkFunction::F8.new
 ]
 
 fs.each do |f|
@@ -26,7 +26,7 @@ fs.each do |f|
     f: f,
     dimension: 20,
     number_of_vectors: 50,
-    max_generation: 300,
+    max_generation: 1000,
     max_evaluation: 100000,
 
     mutation_method: 'current-to-pbest/1',
@@ -54,7 +54,7 @@ fs.each do |f|
     weight: 0.2
   }
 
-  trial_num = 7 # Assume this is odd
+  trial_num = 9 # Assume this is odd
   center_index = (trial_num - 1) / 2
   p = DE::Plotter.new
   p2 = DE::Plotter.new
@@ -63,29 +63,27 @@ fs.each do |f|
   jades.each(&:exec)
   median_jade = jades.sort { |j| j.min_vectors.last.calculated_value }[center_index]
   jade_min = median_jade.min_vectors.map(&:calculated_value)
-  p.add_min_value_transition('jade', jade_min)
-  # p.add_parameters_transition('jade params', median_jade.parameter_all_history)
-  # p2.add_parameters_transition('jade', median_jade.parameter_all_history)
-  p2.add_parameter_transition('jade', median_jade.parameter_mean_history)
+  p.add_min_value_transition('JADE', jade_min)
+  p2.add_parameter_transition('JADE', median_jade.parameter_mean_history)
   p2.plot_parameter_transitions(title: f.label, plot_c: false)
   p2.plot_parameter_transitions(title: f.label, plot_f: false)
 
-  0.step(1, 0.2) do |weight|
+  0.step(0.5, 0.1) do |weight|
     p3 = DE::Plotter.new
     weight = weight.round(1)
-    next unless weight == 0.2 || weight == 0.6 || weight == 0.0
+    # next unless weight == 0.2 || weight == 0.6 || weight == 0.0
     weight_changed_options = options.merge(weight: weight)
     djades = Array.new(trial_num) { DJADE.new(weight_changed_options) }
     djades.each(&:exec)
     median_djade = djades.sort { |j| j.min_vectors.last.calculated_value }[center_index]
     djade_min = median_djade.min_vectors.map(&:calculated_value)
-    p.add_min_value_transition("djade w: #{weight}", djade_min)
+    p.add_min_value_transition("DJADE w: #{weight}", djade_min)
     p3.add_parameter_transition("mean", median_djade.parameter_mean_history)
     p3.add_parameter_transition("success mean", median_djade.parameter_success_history)
     p3.add_parameter_transition("fail mean", median_djade.parameter_fail_history)
-    # p3.add_parameters_transition("djade w: #{weight}", median_djade.parameter_all_history)
-    p3.plot_parameter_transitions(title: f.label + " djade w: #{weight}", plot_f: false)
-    p3.plot_parameter_transitions(title: f.label + " djade w: #{weight}", plot_c: false)
+    # p3.add_parameters_transition("DJADE w: #{weight}", median_djade.parameter_all_history)
+    p3.plot_parameter_transitions(title: f.label + " DJADE w: #{weight}", plot_f: false)
+    p3.plot_parameter_transitions(title: f.label + " DJADE w: #{weight}", plot_c: false)
   end
 
   p.plot_min_value_transitions("#{f.label}")
