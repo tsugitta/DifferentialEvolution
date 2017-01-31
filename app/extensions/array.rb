209496e59176a4e52c8e330d19ce692e6e490be3
Toshinori Tsugita <tsugita.dev@gmail.com>
2017-01-31 14:52:45 +0900

    Array can get one-fourth vaule

--- app/extensions/array.rb
+++ app/extensions/array.rb
@@ -16,4 +16,11 @@ class Array
     sorted = self.sort
     size % 2 == 0 ? sorted[size / 2 - 1, 2].inject(:+) / 2.0 : sorted[size / 2]
   end
+
+  def one_fourth
+    sorted = self.sort
+    prev = sorted.select { |e| e < median }
+    return first if prev.empty?
+    prev.median
+  end
 end
